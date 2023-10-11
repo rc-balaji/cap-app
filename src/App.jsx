e9762a068e902c5e53c4b35b4894a02db39a37c6
rc-balaji <balajic@drngpit.ac.in>
2023-10-11 19:43:29 +0530

    FInish

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react'
+import { List } from './components/List'
+import axios from 'axios'
+import { baseURL } from './utils/constant'
 
 function App() {
-  const [num1, setNum1] = useState('');
-  const [num2, setNum2] = useState('');
-  const [result, setResult] = useState(0);
+  
+  const [task1, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [updateUI, setUpdateUI] = useState(false)
+  const [updateID, setUpdateID] = useState(null)
+  const [refresh,setRefresh] =useState(0)
 
-  const addNumbers = () => {
-    const sum = parseFloat(num1) + parseFloat(num2);
-    setResult(sum);
-  };
+  const Get = async ()=>{
+    await axios.get(`${baseURL}/get`).then((res) => {
+      console.log(res.data)
+      setTasks(res.data)
+    })
+    setRefresh(refresh+1)
+  }
+  
+  useEffect( () => {
+    Get()
+  }, [updateUI,refresh])
 
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, { task: task1 }).then((res) => {
+      console.log(res.data)
+      setTask("")
+      setUpdateUI((prevState) => !prevState)
+    })
+  }
+  
+  const updateTask = () => {
+    axios.put(`${baseURL}/update/${updateID}`, { task: task1 }).then((res) => {
+      console.log(res.data)
+      setTask("")
+      setUpdateUI((prevState) => !prevState)
+    })
+  }
+  
+  const updateMode = (id, text) => {
+    console.log(text);
+    setTask(text)
+    setUpdateID(id)
+  }
+  
   return (
     <div>
-      <h1>Add Two Numbers</h1>
-      <input
-        type="number"
-        placeholder="Enter number 1"
-        value={num1}
-        onChange={(e) => setNum1(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Enter number 2"
-        value={num2}
-        onChange={(e) => setNum2(e.target.value)}
-      />
-      <button onClick={addNumbers}>Add</button>
-      <p>Result: {result}</p>
+      <h1 style={{ fontSize: '24px', textAlign: 'center' }}>TO-DO List</h1>
+      <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+        <input
+          type="text"
+          value={task1}
+          onChange={(e) => {
+            setTask(e.target.value)
+          }}
+        />
+        <button
+          type="submit"
+          onClick={updateID ? updateTask : addTask}
+          style={{
+            marginLeft: '10px',
+            padding: '5px 10px',
+            backgroundColor: '#007bff',
+            color: '#fff',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          {updateID ? "Update Task" : "Add Task"}
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <List
+            key={task._id}
+            id={task._id}
+            task={task.task}
+            updateMode={updateMode}
+            setUpdateUI={setUpdateUI}
+          />
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/utils/constant.jsx
@@ -0,0 +1,1 @@
+export const baseURL = "https://mern-server2-7l54.onrender.com/api"
